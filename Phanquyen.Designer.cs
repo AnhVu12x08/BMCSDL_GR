d28813df1db73708c1ab899d00829fa24a778510
AnhVu12x08 <truonganhvu120804@gmail.com>
2024-11-10 11:59:38 +0700

    add items to combobox

--- Phanquyen.Designer.cs
+++ Phanquyen.Designer.cs
@@ -30,21 +30,23 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.cbbGranterSys = new System.Windows.Forms.ComboBox();
-            this.cmbGranteeSys = new System.Windows.Forms.ComboBox();
-            this.checklistObject = new System.Windows.Forms.CheckedListBox();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
-            this.comboBox4 = new System.Windows.Forms.ComboBox();
-            this.comboBox5 = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checklistObject = new System.Windows.Forms.CheckedListBox();
+            this.cbbGranteeSys = new System.Windows.Forms.ComboBox();
+            this.cbbGranterSys = new System.Windows.Forms.ComboBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbbObjectObj = new System.Windows.Forms.ComboBox();
+            this.cbbGranteeObj = new System.Windows.Forms.ComboBox();
+            this.cbbGranterObj = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
@@ -62,11 +64,12 @@
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.button3);
             this.tabPage1.Controls.Add(this.button2);
             this.tabPage1.Controls.Add(this.label5);
             this.tabPage1.Controls.Add(this.label4);
             this.tabPage1.Controls.Add(this.checklistObject);
-            this.tabPage1.Controls.Add(this.cmbGranteeSys);
+            this.tabPage1.Controls.Add(this.cbbGranteeSys);
             this.tabPage1.Controls.Add(this.cbbGranterSys);
             this.tabPage1.Location = new System.Drawing.Point(4, 25);
             this.tabPage1.Name = "tabPage1";
@@ -76,39 +79,33 @@
             this.tabPage1.Text = "System Privileges";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // button2
             // 
-            this.tabPage2.Controls.Add(this.button1);
-            this.tabPage2.Controls.Add(this.checkedListBox2);
-            this.tabPage2.Controls.Add(this.label3);
-            this.tabPage2.Controls.Add(this.label2);
-            this.tabPage2.Controls.Add(this.label1);
-            this.tabPage2.Controls.Add(this.comboBox5);
-            this.tabPage2.Controls.Add(this.comboBox4);
-            this.tabPage2.Controls.Add(this.comboBox3);
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(993, 564);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Object Privileges";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.button2.Location = new System.Drawing.Point(854, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Back";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.clickBack);
             // 
-            // cbbGranterSys
+            // label5
             // 
-            this.cbbGranterSys.FormattingEnabled = true;
-            this.cbbGranterSys.Location = new System.Drawing.Point(33, 60);
-            this.cbbGranterSys.Name = "cbbGranterSys";
-            this.cbbGranterSys.Size = new System.Drawing.Size(121, 24);
-            this.cbbGranterSys.TabIndex = 0;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(272, 41);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Grantee";
             // 
-            // cmbGranteeSys
+            // label4
             // 
-            this.cmbGranteeSys.FormattingEnabled = true;
-            this.cmbGranteeSys.Location = new System.Drawing.Point(33, 114);
-            this.cmbGranteeSys.Name = "cmbGranteeSys";
-            this.cmbGranteeSys.Size = new System.Drawing.Size(121, 24);
-            this.cmbGranteeSys.TabIndex = 1;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Granter";
             // 
             // checklistObject
             // 
@@ -128,92 +125,46 @@
             "UPDATE ANY TABLE",
             "DELETE ANY TABLE",
             "EXECUTE ANY PROCEDURE"});
-            this.checklistObject.Location = new System.Drawing.Point(33, 161);
+            this.checklistObject.Location = new System.Drawing.Point(533, 60);
             this.checklistObject.Name = "checklistObject";
-            this.checklistObject.Size = new System.Drawing.Size(245, 259);
+            this.checklistObject.Size = new System.Drawing.Size(245, 327);
             this.checklistObject.TabIndex = 2;
             // 
-            // comboBox3
+            // cbbGranteeSys
             // 
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(68, 83);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(121, 24);
-            this.comboBox3.TabIndex = 0;
+            this.cbbGranteeSys.FormattingEnabled = true;
+            this.cbbGranteeSys.Location = new System.Drawing.Point(275, 60);
+            this.cbbGranteeSys.Name = "cbbGranteeSys";
+            this.cbbGranteeSys.Size = new System.Drawing.Size(179, 24);
+            this.cbbGranteeSys.TabIndex = 1;
+            this.cbbGranteeSys.SelectedIndexChanged += new System.EventHandler(this.cbbGranteeSys_SelectedIndexChanged);
             // 
-            // comboBox4
+            // cbbGranterSys
             // 
-            this.comboBox4.FormattingEnabled = true;
-            this.comboBox4.Location = new System.Drawing.Point(67, 143);
-            this.comboBox4.Name = "comboBox4";
-            this.comboBox4.Size = new System.Drawing.Size(121, 24);
-            this.comboBox4.TabIndex = 1;
+            this.cbbGranterSys.FormattingEnabled = true;
+            this.cbbGranterSys.Location = new System.Drawing.Point(33, 60);
+            this.cbbGranterSys.Name = "cbbGranterSys";
+            this.cbbGranterSys.Size = new System.Drawing.Size(159, 24);
+            this.cbbGranterSys.TabIndex = 0;
             // 
-            // comboBox5
+            // tabPage2
             // 
-            this.comboBox5.FormattingEnabled = true;
-            this.comboBox5.Location = new System.Drawing.Point(67, 199);
-            this.comboBox5.Name = "comboBox5";
-            this.comboBox5.Size = new System.Drawing.Size(121, 24);
-            this.comboBox5.TabIndex = 2;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(65, 64);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 16);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Granter";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(64, 124);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(55, 16);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Grantee";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(64, 180);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(46, 16);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Object";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(30, 41);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(51, 16);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Granter";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(30, 95);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(55, 16);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "Grantee";
-            // 
-            // checkedListBox2
-            // 
-            this.checkedListBox2.FormattingEnabled = true;
-            this.checkedListBox2.Items.AddRange(new object[] {
-            "SELECT",
-            "INSERT",
-            "UPDATE",
-            "DELETE"});
-            this.checkedListBox2.Location = new System.Drawing.Point(67, 253);
-            this.checkedListBox2.Name = "checkedListBox2";
-            this.checkedListBox2.Size = new System.Drawing.Size(121, 89);
-            this.checkedListBox2.TabIndex = 6;
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.checkedListBox2);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.cbbObjectObj);
+            this.tabPage2.Controls.Add(this.cbbGranteeObj);
+            this.tabPage2.Controls.Add(this.cbbGranterObj);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(993, 564);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Object Privileges";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
@@ -225,15 +176,88 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.clickBack);
             // 
-            // button2
+            // checkedListBox2
             // 
-            this.button2.Location = new System.Drawing.Point(854, 19);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 8;
-            this.button2.Text = "Back";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.clickBack);
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Items.AddRange(new object[] {
+            "SELECT",
+            "INSERT",
+            "UPDATE",
+            "DELETE"});
+            this.checkedListBox2.Location = new System.Drawing.Point(32, 236);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.checkedListBox2.Size = new System.Drawing.Size(181, 106);
+            this.checkedListBox2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(544, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Object";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(279, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Grantee";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Granter";
+            // 
+            // cbbObjectObj
+            // 
+            this.cbbObjectObj.FormattingEnabled = true;
+            this.cbbObjectObj.Location = new System.Drawing.Point(547, 83);
+            this.cbbObjectObj.Name = "cbbObjectObj";
+            this.cbbObjectObj.Size = new System.Drawing.Size(201, 24);
+            this.cbbObjectObj.TabIndex = 2;
+            // 
+            // cbbGranteeObj
+            // 
+            this.cbbGranteeObj.FormattingEnabled = true;
+            this.cbbGranteeObj.Location = new System.Drawing.Point(282, 83);
+            this.cbbGranteeObj.Name = "cbbGranteeObj";
+            this.cbbGranteeObj.Size = new System.Drawing.Size(201, 24);
+            this.cbbGranteeObj.TabIndex = 1;
+            // 
+            // cbbGranterObj
+            // 
+            this.cbbGranterObj.FormattingEnabled = true;
+            this.cbbGranterObj.Location = new System.Drawing.Point(32, 83);
+            this.cbbGranterObj.Name = "cbbGranterObj";
+            this.cbbGranterObj.Size = new System.Drawing.Size(200, 24);
+            this.cbbGranterObj.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(224, 395);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(399, 366);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // Phanquyen
             // 
@@ -257,13 +281,13 @@
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
-        private System.Windows.Forms.ComboBox cmbGranteeSys;
+        private System.Windows.Forms.ComboBox cbbGranteeSys;
         private System.Windows.Forms.ComboBox cbbGranterSys;
         private System.Windows.Forms.CheckedListBox checklistObject;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox5;
-        private System.Windows.Forms.ComboBox comboBox4;
-        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox cbbObjectObj;
+        private System.Windows.Forms.ComboBox cbbGranteeObj;
+        private System.Windows.Forms.ComboBox cbbGranterObj;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label5;
@@ -271,5 +295,7 @@
         private System.Windows.Forms.CheckedListBox checkedListBox2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
--- Phanquyen.cs
+++ Phanquyen.cs
@@ -17,19 +17,105 @@ namespace Connect_Oracle
         public Phanquyen()
         {
             InitializeComponent();
+            AddGranter("cbbGranterSys");
+            AddGranter("cbbGranteeSys");
+            AddGranter("cbbGranterObj");
+            AddGranter("cbbGranteeObj");
+
         }
 
         //select all users from database and add it to combobox
-        private void AddGranter()
+        private void AddGranter(string componentName)
         {
-            string query = "SELECT USERNAME FROM ALL_USERS";
-            OracleCommand cmd = new OracleCommand(query, DangNhap.conn);
-            OracleDataReader dr = cmd.ExecuteReader();
-            while (dr.Read())
+            // Get the ComboBox control based on its name
+            ComboBox comboBox = this.Controls.Find(componentName, true).FirstOrDefault() as ComboBox;
+
+
+            if (comboBox == null)
             {
-                cbbGranterSys.Items.Add(dr[0]);
+                MessageBox.Show($"Error: ComboBox '{componentName}' not found.");
+                return; 
+            }
+
+            try
+            {
+                using (OracleConnection connection = Database.Get_Connect())
+                {
+                    if (connection != null)
+                    {
+                        using (OracleCommand cmd = new OracleCommand("SELECT USERNAME FROM ALL_USERS", connection))
+                        using (OracleDataReader dr = cmd.ExecuteReader())
+                        {
+                            while (dr.Read())
+                            {
+                                comboBox.Items.Add(dr[0].ToString()); 
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Not connected to the database!");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
             }
         }
+
+        //show table of chosen granter and show to obbject combobox
+
+        private void cbbGranterSys_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbbGranterObj.SelectedItem == null) return;
+
+
+            string granter = cbbGranterSys.SelectedItem.ToString();
+            cbbObjectObj.Items.Clear();
+
+
+            try
+            {
+                using (OracleConnection connection = Database.Get_Connect())
+                {
+                    if (connection != null)
+                    {
+                        // Use UPPER() in the query as well for case-insensitive comparison
+                        string query = $"SELECT TABLE_NAME FROM ALL_TABLES WHERE UPPER(OWNER) = '{granter}'";
+                        using (OracleCommand cmd = new OracleCommand(query, connection))
+                        using (OracleDataReader dr = cmd.ExecuteReader())
+                        {
+                            if (!dr.HasRows)
+                            {
+                                MessageBox.Show($"User '{granter}' does not own any tables.");
+                            }
+                            else
+                            {
+                                while (dr.Read())
+                                {
+                                    cbbObjectObj.Items.Add(dr[0].ToString());
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Not connected to the database!");
+                    }
+                }
+            }
+            catch (OracleException ex) // Catch specific Oracle exceptions
+            {
+                MessageBox.Show($"Oracle Error: {ex.Message}");
+            }
+            catch (Exception ex) // Catch other exceptions
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+
 
 
         private void clickBack(object sender, EventArgs e)
@@ -37,6 +123,11 @@ namespace Connect_Oracle
             this.Hide();
             LuaChon lc = new LuaChon();
             lc.Show();
+        }
+
+        private void cbbGranteeSys_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
